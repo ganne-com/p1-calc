67feaf59fea5ac1262a33cabb5b8f368229da1f5
isaalp <isaalp@users.noreply.github.com>
2018-03-27 23:48:34 +0300

    Update bundle.js

--- pc1/bundle.js
+++ pc1/bundle.js
@@ -419,7 +419,7 @@
 	         cw.colourweeks(0, week);
 	     }
 	     if (d == 5 || d == 6){
-	         recommend.text("Umarız şu ana kadar sorunsuz bir gebelik geçirmişsindir. Bugün itibari ile " + week + ". haftanın son günlerindesiniz. " + week + ". hafta ile ilgili bilgileri tekrar gözden geçirmenizi tavsiye ederiz. Ayrıca sayfanın sonundan " + (week+1) + ". haftaya giderek ilgili haftanın bilgilerini okuyarak yeni haftanıza hazır başlayabilirsiniz.");
+	         recommend.text("Umarız şu ana kadar sorunsuz bir gebelik geçirmişsindir. Bugün itibari ile " + week + ". haftanın son günlerindesiniz. " + week + ". hafta ile ilgili bilgileri tekrar gözden geçirmenizi tavsiye ederiz. Ayrıca sayfanın sonundan xxxx. haftaya giderek ilgili haftanın bilgilerini okuyarak yeni haftanıza hazır başlayabilirsiniz.");
 	         recommendli.show();
 	         cw.colourweeks(week, (week+1));
 	     }
