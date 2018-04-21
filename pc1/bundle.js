aa6b6c8159a72210f689401d1ce30488fb2332d7
isaalp <isaalp@users.noreply.github.com>
2018-04-21 22:45:06 +0300

    Update bundle.js

--- pc1/bundle.js
+++ pc1/bundle.js
@@ -402,7 +402,7 @@
 	}
 
 	function recommendnormal(recommend, recommendli, w, d){
-	     var week = w + 1;
+	     var week = w ;
 	     if (d == 0){
 	         recommend.text("Sayfa sonundan " + week + ". haftaya giderek haftanın bilgilerini okumanızı öneririz. ");
 	         recommendli.show();
